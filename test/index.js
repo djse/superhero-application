ba2e7018aad9b9fd9d53334e9b4fe23abc7b56b5
Jackmc <jackmc1122@gmail.com>
2019-04-29 16:59:09 -0700

    Update travis and index.js to fix travis tests.

--- test/index.js
+++ test/index.js
@@ -5,6 +5,7 @@ const { JSDOM } = jsdom;
 const { window } = new JSDOM(``);
 global.window = window;
 global.document = window.document;
+global.FormData = window.FormData;
 
 require = require('esm')(module);
 module.exports = require('./tests.js');
